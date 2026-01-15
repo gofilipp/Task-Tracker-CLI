fbf355b036f02d235fb93a62d7b7fb01d9d889e6
gofilipp <phlstrgit@gmail.com>
2026-01-15 22:57:27 +0300

    finish(nid a lot of fixes)

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"regexp"
@@ -19,13 +21,14 @@ type TasksList struct {
 
 type ListEditing interface {
 	Add(Task)
-	// Delete(int)
+	Delete(int)
+	ReadFromJSON([]byte)
 }
 
 type TaskMethods interface {
 	MarkDone()
 	MarkInProgress()
-	Update()
+	Update(string)
 }
 
 type Task struct {
@@ -40,30 +43,84 @@ func (t *Task) Update(s string) {
 	t.Description = s
 }
 
+func (t *Task) MarkInProgress() {
+	t.Status = "in-progress"
+	t.UpdatedAt = time.Now().Format("2006-01-02 15:04:05")
+}
+
+func (t *Task) MarkDone() {
+	t.Status = "done"
+	t.UpdatedAt = time.Now().Format("2006-01-02 15:04:05")
+}
+
+func (t *TasksList) ReadFromJSON(JSON []byte) {
+	err := json.Unmarshal(JSON, t)
+	if err != nil {
+		fmt.Println("Ошибка при анмаршале!")
+		return
+	}
+}
+
 func (t *TasksList) Add(task Task) {
 	t.List = append(t.List, task)
+}
+
+func (t *TasksList) Delete(id int) {
+	t.List = append(t.List[:id], t.List[id+1:]...)
 }
 
 func helpText() {
 	fmt.Println(`Выберете команду из списка ниже:
 1. add "[description of task]"
 2. update [id] "[description of task]"
-2. list // all tasks
-3. list done
-4. list todo
-5. list in-progress
+2. list // show all tasks
+3. list [done/todo/in-progress]
+4. delete [index]
+5. mark-[done/in-progress] [id]
+6. exit
 `)
 }
 
+func checkStatusAndPrint(tasks TasksList, status string) {
+	count := 1
+	for _, v := range tasks.List {
+		if v.Status == status {
+			fmt.Printf("%d. %s \n", count, v.Description)
+			count += 1
+		}
+	}
+	if count == 1 {
+		fmt.Println("Список задач со статусом ", status, " пуст")
+	}
+}
+
 func main() {
+	const jsonFilePath = "./tasks.json"
+
+	var file *os.File
 	var tasks TasksList
 
-	// Create JSON file with tasks
-	file, err := os.Create("tasks.json")
-	if err != nil {
-		log.Fatal(err)
+	fmt.Println(os.Stat(jsonFilePath))
+
+	if _, err := os.Stat(jsonFilePath); err != nil {
+		if os.IsNotExist(err) {
+			file, err = os.Create(jsonFilePath)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			panic(err)
+		}
+	} else {
+		file, err = os.OpenFile(jsonFilePath, os.O_RDWR, 0)
+		if err != nil {
+			log.Fatal(err)
+		}
+		byteValue, _ := io.ReadAll(file)
+		tasks.ReadFromJSON(byteValue)
 	}
-	file.Close()
+
+	defer file.Close()
 
 	fmt.Println("Вас приветствует таск-менеджер.")
 	helpText()
@@ -81,7 +138,7 @@ func main() {
 
 		switch keyWord {
 		case "add":
-			re := regexp.MustCompile(`^(\w*)\s"(.+)"$`)
+			re := regexp.MustCompile(`^(add)\s"(.+)"$`)
 			matches := re.FindStringSubmatch(inputCommand)
 			if len(matches) == 0 {
 				fmt.Println("Что-то пошло не так. Напишите /help для показа всех доступных команд.")
@@ -95,24 +152,13 @@ func main() {
 				UpdatedAt:   time.Now().Format("2006-01-02 15:04:05"),
 			})
 			fmt.Println("Задача успешно добавлена!")
-		case "list":
+		case "delete":
 			if len(tasks.List) == 0 {
 				fmt.Println("Список задач пуст!")
 				helpText()
 				break
 			}
-			fmt.Println("Список всех доступных задач: ")
-			for i, v := range tasks.List {
-				fmt.Printf("%d. %s \n", i+1, v.Description)
-			}
-		case "update":
-			if len(tasks.List) == 0 {
-				fmt.Println("Список задач пуст!")
-				helpText()
-				break
-			}
-
-			re := regexp.MustCompile(`^(\w*)\s(\d+)\s"(.+)"$`)
+			re := regexp.MustCompile(`^(delete)\s(\d+)$`)
 			matches := re.FindStringSubmatch(inputCommand)
 
 			if len(matches) == 0 {
@@ -126,15 +172,144 @@ func main() {
 				log.Fatal(err)
 			}
 
-			fmt.Println("ID: ", id, "LEN ", len(tasks.List))
+			if len(tasks.List) < id || id-1 < 0 {
+				fmt.Println("Невозможно удалить задачу под данным индексом, или же такого индекса вовсе нет!")
+				break
+			}
 
-			if id > len(tasks.List) {
+			tasks.Delete(id - 1)
+			fmt.Printf("Задача под индексом %d была успешно удалена!\nВесь список команд - /help\n", id)
+		case "list":
+			if len(tasks.List) == 0 {
+				fmt.Println("Список задач пуст!")
+				helpText()
+				break
+			}
+			re := regexp.MustCompile(`^(list)\s(.+)$`)
+			matches := re.FindStringSubmatch(inputCommand)
+
+			if len(matches) == 3 {
+				switch matches[2] {
+				case "done":
+					checkStatusAndPrint(tasks, "done")
+				case "todo":
+					checkStatusAndPrint(tasks, "todo")
+				case "in-progress":
+					checkStatusAndPrint(tasks, "in-progress")
+				default:
+					fmt.Println("Нет такой команды. Весь список команд - /help")
+				}
+			} else if len(matches) == 0 {
+				fmt.Println("Список всех задач: ")
+				for i, v := range tasks.List {
+					fmt.Printf("%d. %s \n", i+1, v.Description)
+				}
+			}
+		case "mark-in-progress":
+			if len(tasks.List) == 0 {
+				fmt.Println("Список задач пуст!")
+				helpText()
+				break
+			}
+
+			re := regexp.MustCompile(`^(mark-in-progress)\s(\d+)$`)
+			matches := re.FindStringSubmatch(inputCommand)
+
+			if len(matches) == 0 {
+				fmt.Println("Что-то пошло не так. Напишите /help для показа всех доступных команд.")
+				break
+			}
+
+			id, err := strconv.Atoi(matches[2])
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if len(tasks.List) < id || id-1 < 0 {
+				fmt.Println("Невозможно изменить задачу под данным индексом, или же такого индекса вовсе нет!")
+				break
+			}
+
+			tasks.List[id-1].MarkInProgress()
+			fmt.Printf("Задача под индексом %d была успешно изменена!\nВесь список команд - /help\n", id)
+		case "mark-done":
+			if len(tasks.List) == 0 {
+				fmt.Println("Список задач пуст!")
+				helpText()
+				break
+			}
+
+			re := regexp.MustCompile(`^(mark-done)\s(\d+)$`)
+			matches := re.FindStringSubmatch(inputCommand)
+
+			if len(matches) == 0 {
+				fmt.Println("Что-то пошло не так. Напишите /help для показа всех доступных команд.")
+				break
+			}
+
+			id, err := strconv.Atoi(matches[2])
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if len(tasks.List) < id || id-1 < 0 {
+				fmt.Println("Невозможно изменить задачу под данным индексом, или же такого индекса вовсе нет!")
+				break
+			}
+
+			tasks.List[id-1].MarkDone()
+			fmt.Printf("Задача под индексом %d была успешно изменена!\nВесь список команд - /help\n", id)
+		case "update":
+			if len(tasks.List) == 0 {
+				fmt.Println("Список задач пуст!")
+				helpText()
+				break
+			}
+
+			re := regexp.MustCompile(`^(update)\s(\d+)\s"(.+)"$`)
+			matches := re.FindStringSubmatch(inputCommand)
+
+			if len(matches) == 0 {
+				fmt.Println("Что-то пошло не так. Напишите /help для показа всех доступных команд.")
+				break
+			}
+
+			id, err := strconv.Atoi(matches[2])
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if id > len(tasks.List) || id-1 < 0 {
 				fmt.Println("Нет задачи с таким ID. Напишите /help для показа всех доступных команд.")
 				break
 			}
 
+			textUpdated := matches[3]
+
+			tasks.List[id-1].Update(textUpdated)
+			fmt.Printf("Задача под индексом %d была успешно обновлена!\nВесь список команд - /help\n", id)
 		case "/help":
 			helpText()
+		case "exit":
+			listToBytes, err := json.Marshal(tasks)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+			_, err = file.Seek(0, io.SeekStart)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			err = file.Truncate(0)
+			if err != nil {
+				log.Fatal(err)
+			}
+			file.Write(listToBytes)
+			return
 		default:
 			fmt.Println("Нет такой команды. Весь список - /help")
 		}
